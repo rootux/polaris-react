10a5095bfd747f9fbe5ad1b06b1329082e1676ab
Laura Aubin <laura.aubin@shopify.com>
2019-11-04 16:10:11 -0500

    [Modal] Modal contents should keep focus after last tab

--- src/components/Focus/tests/Focus.test.tsx
+++ src/components/Focus/tests/Focus.test.tsx
@@ -31,7 +31,7 @@ describe('<Focus />', () => {
     expect(input).toBe(document.activeElement);
   });
 
-  it('will not focus the first focusable node is the `disabled` is true', () => {
+  it('will not focus the first focusable node if `disabled` is true', () => {
     const focus = mountWithAppProvider(
       <FocusTestWrapper disabled>
         <input />
